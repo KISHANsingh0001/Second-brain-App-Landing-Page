6026c460bfa487b87ab59469099ec413d024a975
Kishan <kishansinghthakur27@gmail.com>
2025-04-28 16:55:14 +0530

    added responsiveness

--- src/components/ui/animated-beam.tsx
+++ src/components/ui/animated-beam.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import React, { useEffect, useState, useRef } from 'react'
 import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
+import ElegantShape from "./elegantShape";
 
 const fadeUpVariants = {
     hidden: { opacity: 0, y: 30 },
@@ -19,71 +20,7 @@ const fadeUpVariants = {
 };
 
 const images = ["/pic2.png", "/pic1.png"];
-export function ElegantShape({
-    className,
-    delay = 0,
-    width = 400,
-    height = 100,
-    rotate = 0,
-    gradient = "from-white/[0.08]",
-}: {
-    className?: string;
-    delay?: number;
-    width?: number;
-    height?: number;
-    rotate?: number;
-    gradient?: string;
-}) {
-    return (
-        <motion.div
-            initial={{
-                opacity: 0,
-                y: -150,
-                rotate: rotate - 15,
-            }}
-            animate={{
-                opacity: 1,
-                y: 0,
-                rotate: rotate,
-            }}
-            transition={{
-                duration: 2.4,
-                delay,
-                ease: [0.23, 0.86, 0.39, 0.96],
-                opacity: { duration: 1.2 },
-            }}
-            className={cn("absolute", className)}
-        >
-            <motion.div
-                animate={{
-                    y: [0, 15, 0],
-                }}
-                transition={{
-                    duration: 12,
-                    repeat: Number.POSITIVE_INFINITY,
-                    ease: "easeInOut",
-                }}
-                style={{
-                    width,
-                    height,
-                }}
-                className="relative"
-            >
-                <div
-                    className={cn(
-                        "absolute inset-0 rounded-full",
-                        "bg-gradient-to-r to-transparent",
-                        gradient,
-                        "backdrop-blur-[2px] border-2 border-white/[0.15]",
-                        "shadow-[0_8px_32px_0_rgba(255,255,255,0.1)]",
-                        "after:absolute after:inset-0 after:rounded-full",
-                        "after:bg-[radial-gradient(circle_at_50%_50%,rgba(255,255,255,0.2),transparent_70%)]"
-                    )}
-                />
-            </motion.div>
-        </motion.div>
-    );
-}
+
 
 export default function AnimatedBeam() {
     const [showGradient, setShowGradient] = useState(false);
@@ -187,75 +124,123 @@ export default function AnimatedBeam() {
 
         //  </div>
     return (
-        <motion.div
-            custom={0}
-            variants={fadeUpVariants}
-            initial="hidden"
-            animate="visible"
-            className="w-full py-14 "
-        >
-
-            <div
-                ref={containerRef}
-                className="relative flex h-[16rem] sm:h-[25.625rem] sm:w-[600px] md:h-[483px] md:w-[800px] 
-                w-full max-w-5xl mx-auto flex-col items-center justify-center overflow-visible border-4"
-            >  
-                {/* Orange Gradient Background */}
-                <div
-                    className={cn(
-                        "absolute inset-0 -top-20 -left-8 -right-8 rounded-5xl bg-[radial-gradient(ellipse_at_center,_rgba(255,165,0,0.5),_rgba(255,165,0,0))] opacity-0 transition-all duration-1000 blur-3xl",
-                        showGradient && "opacity-100",
-                    )}
-                />
-
-                {/* Main Content Container */}
-                
-                <div
-                    className="w-full h-full mx-4 rounded-lg shadow-xl overflow-hidden  border-4 border-white "
-                >
-                    <div
-                        ref={carouselRef}
-                        className="w-full h-full flex  overflow-x-auto scroll-smooth"
-                        style={{ scrollbarWidth: 'none' }}
-                    >
-                        {images.map((src, index) => (
-                            <div
-                                key={index}
-                                className="w-full h-full flex-shrink-0 snap-start relative"
-                            >
-                                <Image
-                                    src={src}
-                                    alt={`Image ${index + 1}`}
-                                    fill
-                                    
-                                    className={`rounded-lg object-contain transition-opacity duration-500 ${index === currentImageIndex ? 'opacity-100' : 'opacity-100'}`}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-               <div className="flex justify-center">
-               <div className="flex">
-                    <div
-
-                        className="cursor-pointer z-10 "
-                        onClick={handlePrev}
-                    >
-                        <ArrowBigLeft color="white" size={40} className="hover:bg-blue-600" />
-                    </div>
-
-                    <div
-                        className=" cursor-pointer z-10"
-                        onClick={handleNext}
-                    >
-                        <ArrowBigRight color="white" size={40} className="hover:bg-blue-600" />
-                    </div>
-
-                </div>
+      <div className="relative w-full py-14">
+         <div className="absolute inset-0 overflow-hidden">
+                 <ElegantShape
+                   delay={0.3}
+                   width={600}
+                   height={140}
+                   rotate={12}
+                   gradient="from-indigo-500/[0.15]"
+                   className="left-[-10%] md:left-[-5%] top-[15%] md:top-[20%]"
+                 />
+         
+                 <ElegantShape
+                   delay={0.5}
+                   width={500}
+                   height={120}
+                   rotate={-15}
+                   gradient="from-rose-500/[0.15]"
+                   className="right-[-5%] md:right-[0%] top-[70%] md:top-[75%]"
+                 />
+         
+                 <ElegantShape
+                   delay={0.4}
+                   width={300}
+                   height={80}
+                   rotate={-8}
+                   gradient="from-violet-500/[0.15]"
+                   className="left-[5%] md:left-[10%] bottom-[5%] md:bottom-[10%]"
+                 />
+         
+                 <ElegantShape
+                   delay={0.6}
+                   width={200}
+                   height={60}
+                   rotate={20}
+                   gradient="from-amber-500/[0.15]"
+                   className="right-[15%] md:right-[20%] top-[10%] md:top-[15%]"
+                 />
+         
+                 <ElegantShape
+                   delay={0.7}
+                   width={150}
+                   height={40}
+                   rotate={-25}
+                   gradient="from-cyan-500/[0.15]"
+                   className="left-[20%] md:left-[25%] top-[5%] md:top-[10%]"
+                 />
                </div>
-            
-        </motion.div>
+     
+    <motion.div
+      custom={0}
+      variants={fadeUpVariants}
+      initial="hidden"
+      animate="visible"
+      className="w-full py-14"
+    >
+      <div
+        ref={containerRef}
+        className="relative flex h-[16rem] sm:h-[25.625rem] sm:w-[600px] md:h-[483px] md:w-[800px] 
+        w-full max-w-5xl mx-auto flex-col items-center justify-center overflow-visible "
+      >
+        {/* Orange Gradient Background */}
+        <div
+          className={cn(
+            "absolute inset-0 -top-20 -left-8 -right-8 rounded-5xl bg-[radial-gradient(ellipse_at_center,_rgba(255,165,0,0.5),_rgba(255,165,0,0))] opacity-0 transition-all duration-1000 blur-3xl",
+            showGradient && "opacity-100"
+          )}
+        />
+
+   
+       
+
+        {/* Main Content Container */}
+        <div className="w-full h-full mx-4 rounded-lg shadow-xl overflow-hidden ">
+          <div
+            ref={carouselRef}
+            className="w-full h-full flex overflow-x-auto scroll-smooth"
+            style={{ scrollbarWidth: "none" }}
+          >
+            {images.map((src, index) => (
+              <div
+                key={index}
+                className="w-full h-full flex-shrink-0 snap-start relative"
+              >
+                <Image
+                  src={src}
+                  alt={`Image ${index + 1}`}
+                  fill
+                  className={`rounded-lg object-contain transition-opacity duration-500 ${
+                    index === currentImageIndex
+                      ? "opacity-100"
+                      : "opacity-100"
+                  }`}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="flex">
+          <div
+            className="cursor-pointer z-10"
+            onClick={handlePrev}
+          >
+            <ArrowBigLeft color="white" size={40} className="hover:bg-blue-600" />
+          </div>
+
+          <div
+            className="cursor-pointer z-10"
+            onClick={handleNext}
+          >
+            <ArrowBigRight color="white" size={40} className="hover:bg-blue-600" />
+          </div>
+        </div>
+      </div>
+    </motion.div>
+    </div>
     )
 }
--- src/components/ui/shape-landing-hero.tsx
+++ src/components/ui/shape-landing-hero.tsx
@@ -7,72 +7,73 @@ import GlowEffectButton from "./button";
 import AnimatedBeam from "./animated-beam";
 import AboutUs from "./AboutUs";
 import IconMarquee from "./cta";
+import ElegantShape from '@/components/ui/elegantShape';
 
-export function ElegantShape({
-  className,
-  delay = 0,
-  width = 400,
-  height = 100,
-  rotate = 0,
-  gradient = "from-white/[0.08]",
-}: {
-  className?: string;
-  delay?: number;
-  width?: number;
-  height?: number;
-  rotate?: number;
-  gradient?: string;
-}) {
-  return (
-    <motion.div
-      initial={{
-        opacity: 0,
-        y: -150,
-        rotate: rotate - 15,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-        rotate: rotate,
-      }}
-      transition={{
-        duration: 2.4,
-        delay,
-        ease: [0.23, 0.86, 0.39, 0.96],
-        opacity: { duration: 1.2 },
-      }}
-      className={cn("absolute", className)}
-    >
-      <motion.div
-        animate={{
-          y: [0, 15, 0],
-        }}
-        transition={{
-          duration: 12,
-          repeat: Number.POSITIVE_INFINITY,
-          ease: "easeInOut",
-        }}
-        style={{
-          width,
-          height,
-        }}
-        className="relative"
-      >
-        <div
-          className={cn(
-            "absolute inset-0 rounded-full",
-            "bg-gradient-to-r to-transparent text-white",
-            gradient,
-            "backdrop-blur-[2px] border-2 border-white/[0.15]",
-            "shadow-[0_8px_32px_0_rgba(255,255,255,0.1)]",
-            "after:absolute after:inset-0 after:rounded-full",
-            "after:bg-[radial-gradient(circle_at_50%_50%,rgba(255,255,255,0.2),transparent_70%)]"
-          )}
-        ></div>
-      </motion.div>
-    </motion.div>
-  );
-}
+// export function ElegantShape({
+//   className,
+//   delay = 0,
+//   width = 400,
+//   height = 100,
+//   rotate = 0,
+//   gradient = "from-white/[0.08]",
+// }: {
+//   className?: string;
+//   delay?: number;
+//   width?: number;
+//   height?: number;
+//   rotate?: number;
+//   gradient?: string;
+// }) {
+//   return (
+//     <motion.div
+//       initial={{
+//         opacity: 0,
+//         y: -150,
+//         rotate: rotate - 15,
+//       }}
+//       animate={{
+//         opacity: 1,
+//         y: 0,
+//         rotate: rotate,
+//       }}
+//       transition={{
+//         duration: 2.4,
+//         delay,
+//         ease: [0.23, 0.86, 0.39, 0.96],
+//         opacity: { duration: 1.2 },
+//       }}
+//       className={cn("absolute", className)}
+//     >
+//       <motion.div
+//         animate={{
+//           y: [0, 15, 0],
+//         }}
+//         transition={{
+//           duration: 12,
+//           repeat: Number.POSITIVE_INFINITY,
+//           ease: "easeInOut",
+//         }}
+//         style={{
+//           width,
+//           height,
+//         }}
+//         className="relative"
+//       >
+//         <div
+//           className={cn(
+//             "absolute inset-0 rounded-full",
+//             "bg-gradient-to-r to-transparent text-white",
+//             gradient,
+//             "backdrop-blur-[2px] border-2 border-white/[0.15]",
+//             "shadow-[0_8px_32px_0_rgba(255,255,255,0.1)]",
+//             "after:absolute after:inset-0 after:rounded-full",
+//             "after:bg-[radial-gradient(circle_at_50%_50%,rgba(255,255,255,0.2),transparent_70%)]"
+//           )}
+//         ></div>
+//       </motion.div>
+//     </motion.div>
+//   );
+// }
 
 export default function HeroGeometric({
   badge = "Your Personal Knowledge Hub",
@@ -201,7 +202,7 @@ export default function HeroGeometric({
         </div>
       </div>
 
-      <div className="absolute inset-0 bg-gradient-to-t from-[#030303] via-transparent to-[#030303]/80 pointer-events-none" />
+      {/* <div className="absolute inset-0 bg-gradient-to-t from-[#030303] via-transparent to-[#030303]/80 " /> */}
 
     </div>
     
