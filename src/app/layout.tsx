8dc2893c12964521e5157e4ecb759249d2c89cae
Kishan <kishansinghthakur27@gmail.com>
2025-08-08 00:58:04 +0530

    Remove service worker registration script from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -131,22 +131,6 @@ export default function RootLayout({
             })
           }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js')
-                    .then(function(registration) {
-                      console.log('SW registered: ', registration);
-                    }, function(registrationError) {
-                      console.log('SW registration failed: ', registrationError);
-                    });
-                });
-              }
-            `,
-          }}
-        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
